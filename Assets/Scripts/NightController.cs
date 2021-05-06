3d9478af6d55564c74a52b4e9af1bf8937ef2033
Domas Ruginis <domeyoriginal@gmail.com>
2021-05-06 17:37:09 +0100

    Moon Implemented, Cursor Locked

---
+++ Assets/Scripts/NightController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightController : MonoBehaviour
+{
+    float angle = 90;
+    float index;
+    float speed = (2 * Mathf.PI) / 2400; //[ / = seconds ]
+
+    public float radius = 500;
+    public float amplitudeY = 500.0f;
+    public float omegaY = 1.0f;
+    public float highestIntensity;
+
+    public GameObject centreOfWorld;
+    public GameObject targetLight;
+    public GameObject Moon;
+    public GameObject[] skyBox;
+
+    private void Awake()
+    {
+        targetLight = GameObject.FindGameObjectWithTag("Moon");
+    }
+
+    private void Start()
+    {
+        highestIntensity = targetLight.GetComponent<Light>().intensity;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateTime();
+    }
+
+    void UpdateTime()
+    {
+        index += Time.deltaTime;
+        angle += speed * Time.deltaTime;
+
+        float x = Mathf.Sin(angle) * radius;
+        float y = Mathf.Abs(amplitudeY * Mathf.Sin((omegaY * index) * speed));
+        float z = Mathf.Cos(angle) * radius;
+
+        //lgiht and moon poisiton and rotation with worldCetre
+        targetLight.transform.position = new Vector3(x, y/2 ,z);
+        targetLight.transform.LookAt(centreOfWorld.transform);
+        Moon.transform.position = targetLight.transform.position;
+        Moon.transform.LookAt(centreOfWorld.transform);
+
+        //light intensity with position of moon
+        targetLight.GetComponent<Light>().intensity = (y / amplitudeY) * highestIntensity;
+
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -7,22 +7,30 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float initialSpeed = 6f;
+    public float speed;
+    public float runningSpeed = 12f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     private void Start()
     {
+        
         Cursor.visible = false;
+
+        speed = initialSpeed;
     }
 
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        //noramlize = doesnt rely on frames,
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
+        #region Movement
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -32,5 +40,17 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+        #endregion
+
+        #region Running
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runningSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+        }
+        #endregion
     }
 }
